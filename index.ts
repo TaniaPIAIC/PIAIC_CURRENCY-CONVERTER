e367f6348c9ab73446a5c5fc71811c668574f4cb
TaniaPIAIC <148679686+TaniaPIAIC@users.noreply.github.com>
2023-11-06 22:29:55 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,54 @@
+import inquirer from "inquirer";
+
+let conversion={
+    "PKR":{
+        "USD":0.0036287103563394,
+        "GBP":0.0037,
+        "PKR":1
+    },
+    "GBP":{
+        "USD":1.21,
+        "PKR":271.79,
+        "GBP":1
+    },
+    "USD":{
+        "PKR":275.58,
+        "GBP":0.83,
+        "USD":1
+    }
+}
+
+const answer :{
+    from:"PKR"|"USD"|"GBP",
+    to:"PKR"|"USD"|"GBP",
+    amount:number
+}
+=await inquirer.prompt([
+    {
+        type:"list",
+        name:"from",
+        choices:["PKR","GBP","USD"],
+        message:"Select your Currency"
+    },
+    {
+        type:"list",
+        name:"to",
+        choices:["PKR","GBP","USD"],
+        message:"Select your conversion Currency"
+    },
+    {
+        type:"number",
+        name:"amount",
+        message:"Enter your conversion amount"
+    }
+    
+]);
+const {from,to,amount} = answer;
+if(from && to && amount){
+    let result = conversion[from][to] * amount
+    console.log(`Your conversion from ${from} to ${to} is ${result}`);
+    
+}else{
+    console.log("Invalid inputs");
+    
+}
